63cf900ad77dba10547c7fb4ee8d017bed9af7a6
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-07-27 12:09:52 -0300

    Display two error messages when the form is submitted incorrectly

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello World!!!");
+const form = document.querySelector("form");
+const emailInput = document.querySelector("input[type='email']");
+const submitBtn = document.querySelector(".submit-btn");
+const erroIcon = document.querySelector(".error-icon");
+const emptyMessage = document.querySelector(".empty-input-message");
+const invalidMessage = document.querySelector(".invalid-input-message");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!invalidMessage.classList.contains("hide")) {
+    invalidMessage.classList.add("hide");
+  }
+  if (emailInput.value === "") {
+    emptyMessage.classList.remove("hide");
+  } else {
+    form.submit();
+  }
+});
+
+emailInput.addEventListener("invalid", (e) => {
+  e.preventDefault();
+  if (!emptyMessage.classList.contains("hide")) {
+    emptyMessage.classList.add("hide");
+  }
+  invalidMessage.classList.remove("hide");
+});

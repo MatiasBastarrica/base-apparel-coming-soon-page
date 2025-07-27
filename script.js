665ebccb07449cf9cd531ea232daada000a74e5a
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-07-27 12:32:34 -0300

    Display an error icon when the form is not properly submitted

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const form = document.querySelector("form");
 const emailInput = document.querySelector("input[type='email']");
 const submitBtn = document.querySelector(".submit-btn");
-const erroIcon = document.querySelector(".error-icon");
+const errorIcon = document.querySelector(".error-icon-container");
 const emptyMessage = document.querySelector(".empty-input-message");
 const invalidMessage = document.querySelector(".invalid-input-message");
+
+function showErrorIcon() {
+  errorIcon.classList.remove("hide");
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,6 +15,7 @@ form.addEventListener("submit", (e) => {
     invalidMessage.classList.add("hide");
   }
   if (emailInput.value === "") {
+    showErrorIcon();
     emptyMessage.classList.remove("hide");
   } else {
     form.submit();
@@ -19,6 +24,7 @@ form.addEventListener("submit", (e) => {
 
 emailInput.addEventListener("invalid", (e) => {
   e.preventDefault();
+  showErrorIcon();
   if (!emptyMessage.classList.contains("hide")) {
     emptyMessage.classList.add("hide");
   }
